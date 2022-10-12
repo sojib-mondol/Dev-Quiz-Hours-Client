93650470afb87297f3bf31458326eca9c23b6173
sojib-sudo <sojibmandol@gmail.com>
2022-10-12 09:56:43 +0600

    404 page done

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
     return (
-        <div>
-            <h2>404 page</h2>
+        <div className='m-5'>
+            <h2 className='mt-2 text-3xl font-semibold	text-gray-500'>404 page not found. Please go back to<Link  to='/'> <span className='text-blue-600 hover:text-lime-500'>Home.</span></Link></h2>
         </div>
     );
 };
